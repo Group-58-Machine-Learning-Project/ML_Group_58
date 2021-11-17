a184626246a5629417200d362143b521db4388c1
John Keaney <keaneyjo@tcd.ie>
2021-11-17 21:40:18 +0000

    All working but metres sqr

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ML_Group_58
 Repository for Machine Learning Group 58 Project
+
+Before trying to run make sure to install the daftlisting API. If your using
+PyCharm hover over the import and select add.
--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello World!")
+
+# Title:
+
+from scraping import pull_properties, format_listing, format_to_csv
+
+# Firstly, should web scrap
+print("Attempting web scapping from daft.ie")
+
+# Returns files in the format:
+# 0: [name:string, sqr:double, beds:int, baths:int, distance:double, BER:double, type:string]
+
+
+def main():
+
+    # Code below searches for new daft listings
+    listings = pull_properties()
+    # Then formats to a csv desirable style [{}]
+    accommodation = format_listing(listings[0], listings[1])
+    # Then inserts into a csv file so we don't have to search each time
+    format_to_csv(accommodation)
+
+
+main()
+
